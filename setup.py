6a5634b71f09377c46ff2cc030ca9b260996cc15
Ashkan Tashk <53473481+ashkantashk@users.noreply.github.com>
2024-07-25 11:15:09 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MIPLS2',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        # List your package dependencies here
+        # e.g., 'numpy', 'pandas',
+    ],
+    author='Ashkan Tashk',
+    author_email='author@example.com',
+    description='Machine learning utility based on PLS2 for imputing missing values',
+    url='https://github.com/ashkantashk/MIPLS2',
+)
